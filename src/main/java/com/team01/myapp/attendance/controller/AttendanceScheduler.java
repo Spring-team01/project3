e8fbe5e49273b3c32ed47dd4841a9d4436934a41
xogml9704 <xogml9704@naver.com>
2023-01-11 16:02:58 +0900

    끝

--- src/main/java/com/team01/myapp/attendance/controller/AttendanceScheduler.java
+++ src/main/java/com/team01/myapp/attendance/controller/AttendanceScheduler.java
@@ -11,12 +11,11 @@ import com.team01.myapp.attendance.service.IAttendanceService;
 
 @Component
 public class AttendanceScheduler {
-	
 	@Autowired
 	IAttendanceService attendanceService;
 	
 	// 23시 59분에 결근으로 학생 추가
-	@Scheduled(cron = "0 59 23 * * 1-7")
+	@Scheduled(cron = "0 00 18 * * 1-7")
 	public void getStudentStatus() {
 		Date date = new Date();
 
@@ -25,6 +24,17 @@ public class AttendanceScheduler {
 		String today = format.format(date);
 		
 		attendanceService.checkOut(today);
-   }
+	}
+	
+	@Scheduled(cron = "0 59 23 * * 1-7")
+	public void getStudentStatus2() {
+		Date date = new Date();
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
+		
+		String today = format.format(date);
+		
+		attendanceService.checkOut2(today);
+	}
 
 }
