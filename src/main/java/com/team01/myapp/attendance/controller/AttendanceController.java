fb173eb75653573137f140dbcc172cfded450ec3
xogml9704 <xogml9704@naver.com>
2023-01-11 09:34:59 +0900

    출석 변경

--- src/main/java/com/team01/myapp/attendance/controller/AttendanceController.java
+++ src/main/java/com/team01/myapp/attendance/controller/AttendanceController.java
@@ -40,73 +40,71 @@ public class AttendanceController {
 		
 		String choice = attendanceService.selectAtterdanceCheck(attDate, userId);
 		
-		if(choice == null) {
-			choice = "미출석";
+		// 현재 시간 생성
+		SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("HH");
+		int time = Integer.parseInt(simpleDateFormat2.format(date));
+		
+		// 시간에 따른 status attendance에 추가
+		if(time == 8) {
+			// 8시 안에 출근을 눌렀을 때
+			attendance.setStatus("출근");
+		} else if (time < 8) {
+			// 8시 전에 출근을 눌렀을 때
+			model.addAttribute("message", "아직 출근 시간 전입니다. 8시 이후에 눌러주세요.");
+			return "/home";
+		} else if(8 < time && time < 14) {
+			// 9시 이후에 출근을 눌렀을 때
+			attendance.setStatus("지각");
+		} else {
+			// 18시 24시까지 시간을 눌렀을 때
+			model.addAttribute("message", "너무 늦은 시간에 누르셨습니다. 휴가 신청만 가능합니다.");
+			return "/home";
 		}
 		
-		if(choice.equals("지각") || choice.equals("출근")) {
-			model.addAttribute("message", "이미 출근을 누르셨습니다.");
-			return "/home";
+		// attendance subjectId 담기
+		int subjectId = (Integer) session.getAttribute("subjectId");
+		attendance.setSubjectId(subjectId);
+		
+		// 비교하기 위한 날짜 생성
+		SimpleDateFormat updateDateFormat = new SimpleDateFormat("YY/MM/DD");
+		String updateDate = updateDateFormat.format(date);
+		
+		// 그날의 행이 없을 때
+		if(choice == null) {
+			attendanceService.insertAttendance(attendance);
+		
+		// 그날의 행이 있을 때 (미출근이거나 휴가 이거나)
 		} else {
-			
-			// 현재 시간 생성
-			SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("HH");
-			int time = Integer.parseInt(simpleDateFormat2.format(date));
-			
-			// 시간에 따른 status attendance에 추가
-			if(time == 8) {
-				// 8시 안에 출근을 눌렀을 때
-				attendance.setStatus("출근");
-			} else if (time < 8) {
-				// 8시 전에 출근을 눌렀을 때
-				model.addAttribute("message", "아직 출근 시간 전입니다. 8시 이후에 눌러주세요.");
-				return "/home";
-			} else if(8 < time && time < 14) {
-				// 9시 이후에 출근을 눌렀을 때
-				attendance.setStatus("지각");
-			} else {
-				// 18시 24시까지 시간을 눌렀을 때
-				model.addAttribute("message", "너무 늦은 시간에 누르셨습니다. 휴가 신청만 가능합니다.");
-				return "/home";
-			}
-			
-			// attendance subjectId 담기
-			int subjectId = (Integer) session.getAttribute("subjectId");
-			attendance.setSubjectId(subjectId);
-			
-			// 비교하기 위한 날짜 생성
-			SimpleDateFormat updateDateFormat = new SimpleDateFormat("YY/MM/DD");
-			String updateDate = updateDateFormat.format(date);
-			
 			if(choice.equals("휴가")) {
 				String checkAttTime = attendanceService.selectAttTime(attDate, userId);
 				
 				if(checkAttTime == null) { 
 					
+					// 휴가인데 출근 시간이 없는데 출근을 누르는 경우 (조퇴일때)
 					attendanceService.updateAttendance(userId, updateDate, choice);
 				} else {
 					model.addAttribute("message", "이미 출근을 누르셨습니다.");
 					return "/home";
-				}
+				} 
 			} else if(choice.equals("미출석")) {
-				if((8 < time && time < 18)) {
+				if(time == 8) {
+					choice = "출근";
+				} else if((8 < time && time < 14)) {
 					choice = "지각";
 				} else {
 					choice = "출근";
 				}
 				attendanceService.updateAttendance(userId, updateDate, choice);
-			} else {
-				attendanceService.insertAttendance(attendance);
-			}
-			
-			String attTime = attendanceService.selectAttTime(attDate, userId);
-			String leaveTime = attendanceService.selectLeaveTime(attDate, userId);
-			
-			session.setAttribute("attTime", attTime);
-			session.setAttribute("leaveTime", leaveTime);
-			
-			return "/home";
+			} 
 		}
+			
+		String attTime = attendanceService.selectAttTime(attDate, userId);
+		String leaveTime = attendanceService.selectLeaveTime(attDate, userId);
+		
+		session.setAttribute("attTime", attTime);
+		session.setAttribute("leaveTime", leaveTime);
+		
+		return "/home";
 	}
 	
 	// 퇴근
